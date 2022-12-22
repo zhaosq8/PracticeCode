9a4d1b7ee49c95071cacfa0f94af03a92a17866b
zhaosq0815 <zhaosq0815@163.com>
2022-12-22 11:47:45 +0800

    first commit

---
+++ web/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2022/12/22 10:48 
+# @Author : zhaosq 
+# @File : __init__.py.py
---
+++ web/demo.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2022/12/22 11:02 
+# @Author : zhaosq 
+# @File : demo.py
